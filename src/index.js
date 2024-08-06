1f1245965094201330dbee239537aab233393966
Emily Garcia <emikgq10@hotmail.com>
2024-08-06 11:19:17 -0600

    versión antes de correr

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { renderTasks } from "./ui";
+import { addTask } from "./task";
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Hacemos visible la lista de tareas
+    renderTasks();
+
+    // Agregrar el evento para la función para agregar tareas
+    document.getElementById("task-form").addEventListener("submit", (e) => {
+        e.preventDefault();
+        const taskInput = document.getElementById("task-input");
+        if(taskInput !== "") {
+            // Agregamos la tarea
+            addTask(taskInput);
+
+            // Volvemos a cargar la lista de tareas
+            renderTasks();
+
+            // Limpiar el input
+            document.getElementById("task-input").value = "";
+        }
+    });
+
+});
